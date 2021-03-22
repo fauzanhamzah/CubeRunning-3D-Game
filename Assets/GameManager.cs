446a4d777776b5c93d44aa469e7d2902b6c3846a
Fauzan Hamzah <fauzanhamzahdev@gmail.com>
2021-03-22 21:41:30 +0700

    Commit Project

---
+++ Assets/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    bool  gamehasEnded = false;
+
+    public float restartDelay = 2f;
+
+    public GameObject completeLevelUI;
+
+    public void CompleteLevel()
+    {
+        completeLevelUI.SetActive(true);
+    }
+
+    
+    public void EndGame()
+    {
+        if (gamehasEnded == false)
+        {
+            gamehasEnded = true;
+            Debug.Log("Game Over");
+            Invoke("Restart", restartDelay);
+        }
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+}
---
+++ Assets/Script/Credits.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Credits : MonoBehaviour
+{
+    
+    public void Quit()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void BackMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
